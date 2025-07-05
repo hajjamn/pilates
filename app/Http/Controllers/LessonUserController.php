7cc7bac19a0034508314d1ac8a94210c032e8499
HajjamN <hajjamn@gmail.com>
2025-07-05 17:43:42 +0200

    ADD: generated resource files for entities

---
+++ app/Http/Controllers/LessonUserController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LessonUser;
+use App\Http\Requests\StoreLessonUserRequest;
+use App\Http\Requests\UpdateLessonUserRequest;
+
+class LessonUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLessonUserRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LessonUser $lessonUser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LessonUser $lessonUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLessonUserRequest $request, LessonUser $lessonUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LessonUser $lessonUser)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/WeeklyAvailabilityController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WeeklyAvailability;
+use App\Http\Requests\StoreWeeklyAvailabilityRequest;
+use App\Http\Requests\UpdateWeeklyAvailabilityRequest;
+
+class WeeklyAvailabilityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreWeeklyAvailabilityRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(WeeklyAvailability $weeklyAvailability)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(WeeklyAvailability $weeklyAvailability)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateWeeklyAvailabilityRequest $request, WeeklyAvailability $weeklyAvailability)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(WeeklyAvailability $weeklyAvailability)
+    {
+        //
+    }
+}
