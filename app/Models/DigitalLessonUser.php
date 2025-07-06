39a9ba031c01fc58f32fa51cbde86b37a4633857
HajjamN <hajjamn@gmail.com>
2025-07-06 14:55:45 +0200

    ADD: removed Role controller, requests, model, factory and policy since they are not needed. Set up the models for intermediate and pivot tables

--- app/Models/DigitalLessonUser.php
+++ app/Models/DigitalLessonUser.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DigitalLessonUser extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'digital_lesson_id',
+        'user_id',
+        'unlocked_at'
+    ];
+
+    protected $casts = [
+        'unlocked_at' => 'datetime'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function digitalLesson()
+    {
+        return $this->belongsTo(DigitalLesson::class);
+    }
 }
