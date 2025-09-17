7df0deba8d60961e39132a8c23de983df16264c8
HajjamN <hajjamn@gmail.com>
2025-09-17 16:56:51 +0200

    FIX: videos loading

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,5 +2,6 @@ import './bootstrap';
 import '~resources/scss/app.scss';
 import * as bootstrap from 'bootstrap';
 import.meta.glob([
-    '../img/**'
-])
+    '../img/**',
+    '../videos/**'
+], {eager: true});
